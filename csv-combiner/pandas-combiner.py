ce172ce03a40d2559794e17e4232ba085682fda7
ManojMS <manojsrinath97@gmail.com>
2022-10-30 16:40:08 -0700

    chunking method added

--- csv-combiner/pandas-combiner.py
+++ csv-combiner/pandas-combiner.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import time
+import os
 
 # accessories_df = pd.read_csv("./fixtures/accessories.csv")
 # clothing_df = pd.read_csv("./fixtures/clothing.csv")
@@ -15,5 +16,33 @@ random1_df["filename"] = "random"
 random1_df["filename"] = "random-1"
 combined = pd.concat([random_df,random1_df])
 combined.to_csv('rand.csv',index=False,header=True)
+to_time = time.time() - start_time
+print(to_time)
+
+# using chunking method 1:
+start_time = time.time()
+files = ["./fixtures/random.csv","./fixtures/random-1.csv"]
+chunks = []
+for file in files:
+    for chunk in pd.read_csv(file,chunksize=10**4):
+        filename = os.path.basename(file)
+        chunk['filename'] = filename
+        chunks.append(chunk)
+for chunk in chunks:
+    print(chunk.to_csv(index=False, header=True, chunksize=10**4, line_terminator='\n'), end='')
+to_time = time.time() - start_time
+print(to_time)
+
+# just pandas method:
+start_time = time.time()
+files = ["./fixtures/random.csv","./fixtures/random-1.csv"]
+chunks = []
+for file in files:
+    filename = os.path.basename(file)
+    chunk = pd.read_csv(file)
+    chunk['filename'] = filename
+    chunks.append(chunk)
+for chunk in chunks:
+    print(chunk.to_csv(index=False, line_terminator='\n'), end='')
 to_time = time.time() - start_time
 print(to_time)
