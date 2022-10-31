3ba4ef828eeb7e6493b8876ee5647f7bb138f443
ManojMS <manojsrinath97@gmail.com>
2022-10-31 11:24:53 -0700

    added consts file

---
+++ csv-combiner/consts.py
@@ -0,0 +1,16 @@
+ENABLE_PRINT = "enable printing on terminal or to a file!"
+ARG_ERROR_MESSAGE = "invalid input format!!\n\
+The command should be any one of these formats:\n\
+python csv-combiner.py file1.csv file2.csv ...\n\
+python csv-combiner.py --folder csv_folder_path\n\
+----------------------------------------\n\
+optional arguments:\n\
+Note: Give all optional params before giving file name arguments\n\
+'-o' or '--output' for output csv file name\n\
+'--no-print' for not printing combined csv on terminal(Note: use this only when you are printing the csv to a file)"
+FILE_ERROR = "file path not exists. Please check if your input csv file path is correct\n\
+Also please check if you are not giving valid optional parameters and any optional parameters after file names. The command format should be like:\n\
+python csv-combiner.py -o op.csv file1.csv file2.csv ...\n\
+valid options are: '-o','--output','--folder','--no-print'"
+WRONG_FOLDER_MESSAGE = "Wrong folder path!!"
+NO_FILE_ERROR = "No CSV files present in the folder specified!"
--- csv-combiner/csv-combiner.py
+++ csv-combiner/csv-combiner.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import os
 import sys, getopt
+import consts as CONSTS
 
-# take command line input
 class CSVCombiner():
 
     def __init__(self):
@@ -15,38 +15,30 @@ class CSVCombiner():
         self.combined_csv = None
 
     def main(self,argv):
-        #arguments validation
         self.validate_args(argv)
-        # process input csv files
         self.process_csv_files()
-        # combine csv files
         self.combine_csv()
         print(self.is_output_file)
         if not self.is_output_file:
             if not self.is_print:
-                print("enable printing on terminal or to a file!")
+                print(CONSTS.ENABLE_PRINT)
                 sys.exit(2)
             self.print_csv()
         else:
             self.create_csv_file()
             if self.is_print:
                 self.print_csv()
-
         return
 
     def process_csv_files(self):
         for file in self.files:
             file_name = os.path.basename(file)
             for chunk in pd.read_csv(file,chunksize=10**4):
-            # chunk = pd.read_csv(file,chunksize=10**4)
                 chunk["filename"] = file_name
                 self.file_chunks.append(chunk)
 
     def print_csv(self):
-        # for chunk in self.file_chunks:
-        #     print(chunk.to_csv(index=False, header=True, chunksize=10**4, line_terminator='\n'), end='')
         print(self.combined_csv.to_csv(index=False, header=True, chunksize=10**4, line_terminator='\n'), end='')
-
 
     def validate_args(self,argv):
         if len(argv) == 0:
@@ -55,15 +47,7 @@ class CSVCombiner():
         try:
             opts, args = getopt.getopt(argv,"o:",["folder=","output=","no-print"])
         except:
-            print("invalid input format!!")
-            print("The command should be any one of these formats:")
-            print("python csv-combiner.py file1.csv file2.csv ...")
-            print("python csv-combiner.py --folder csv_folder_path")
-            print("----------------------------------------")
-            print("optional arguments:")
-            print("Note: Give all optional params before giving file name arguments")
-            print("'-o' or '--output' for output csv file name")
-            print("'--no-print' for not printing combined csv on terminal(Note: use this only when you are printing the csv to a file)")
+            print(CONSTS.ARG_ERROR_MESSAGE)
             sys.exit(2)
         
         files = args
@@ -78,11 +62,11 @@ class CSVCombiner():
             elif opt_name == "--folder":
                 self.folder = opt_val
                 if not os.path.exists(opt_val):
-                    print("Wrong folder path!!")
+                    print(CONSTS.WRONG_FOLDER_MESSAGE)
                     sys.exit(2)
                 csv_files = [f for f in os.listdir(self.folder) if f.endswith('.csv')]
                 if len(csv_files) <= 0:
-                    print("No CSV files present in the folder specified!")
+                    print(CONSTS.NO_FILE_ERROR)
                     sys.exit(2)
                 for csv_file in csv_files:
                     if self.folder[-1] == '/':
@@ -96,10 +80,7 @@ class CSVCombiner():
         # check if files are present
         for file in files:
             if not os.path.exists(file):
-                print("file path not exists. Please check if your input csv file path is correct")
-                print("Also please check if you are not giving valid optional parameters and any optional parameters after file names. The command format should be like:")
-                print("python csv-combiner.py -o op.csv file1.csv file2.csv ...")
-                print("valid options are: '-o','--output','--folder','--no-print'")
+                print(CONSTS.FILE_ERROR)
                 sys.exit(2)
             self.files.append(file)
         
@@ -107,10 +88,7 @@ class CSVCombiner():
         self.combined_csv = pd.concat(self.file_chunks)
 
     def create_csv_file(self):
-        # combined = pd.concat(self.file_chunks)
         self.combined_csv.to_csv(self.output_file+".csv",index=False,header=True)
-
-
 
 if __name__ == '__main__':
     obj = CSVCombiner()
