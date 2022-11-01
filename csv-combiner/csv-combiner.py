3baa68431550fc0b38820da783585648aec40fca
ManojMS <manojsrinath97@gmail.com>
2022-10-31 21:31:45 -0700

    Added comments

--- csv-combiner/csv-combiner.py
+++ csv-combiner/csv-combiner.py
@@ -3,6 +3,14 @@ import os
 import sys, getopt
 import consts as CONSTS
 
+##########################################################################################
+# Class to combine CSV files into single CSV                                             #
+# Properties:                                                                            #
+# files, folder, output_file, is_print, is_file_output, file_chunks, combined_csv        #
+# methods:                                                                               #
+# validate_args, process_csv_files, combine_csv, output_csv, print_csv, create_csv_file  #
+# validate_and_process_files, validate_and_process_command_options                       #
+##########################################################################################
 class CSVCombiner():
 
     def __init__(self):
@@ -10,7 +18,7 @@ class CSVCombiner():
         self.folder = ""
         self.output_file = ""
         self.is_print = True
-        self.is_output_file = False
+        self.is_file_output = False
         self.file_chunks = []
         self.combined_csv = None
 
@@ -21,8 +29,12 @@ class CSVCombiner():
         self.output_csv()
         return
 
+    ##########################################################################################
+    # Function to handle output of combined CSV                                              #
+    # By default console printing is enabled                                                 #
+    ##########################################################################################
     def output_csv(self):
-        if not self.is_output_file:
+        if not self.is_file_output:
             if not self.is_print:
                 print(CONSTS.ENABLE_PRINT)
                 sys.exit(2)
@@ -32,6 +44,9 @@ class CSVCombiner():
             if self.is_print:
                 self.print_csv()
 
+    ##########################################################################################
+    # function to read CSV files in chunk and add filename column to it                      #
+    ##########################################################################################
     def process_csv_files(self):
         for file in self.files:
             file_name = os.path.basename(file)
@@ -39,9 +54,15 @@ class CSVCombiner():
                 chunk["filename"] = file_name
                 self.file_chunks.append(chunk)
 
+    ##########################################################################################
+    # Function to pring the combined CSV on terminal                                         #
+    ########################################################################################## 
     def print_csv(self):
         print(self.combined_csv.to_csv(index=False, header=True, chunksize=10**4, line_terminator='\n'), end='')
 
+    ##########################################################################################
+    # Function to validate the arguments and options passed by user on terminal              #
+    ##########################################################################################
     def validate_args(self,argv):
         if len(argv) == 0:
             print("Enter csv files to be combined!")
@@ -54,6 +75,9 @@ class CSVCombiner():
         self.validate_and_process_command_options(opts)
         self.validate_and_process_files(files)
         
+    ##########################################################################################
+    # Function to validate the input CSV files                                               #
+    ##########################################################################################
     def validate_and_process_files(self,files):
         for file in files:
             if not os.path.exists(file):
@@ -61,11 +85,14 @@ class CSVCombiner():
                 sys.exit(2)
             self.files.append(file)
     
+    ##########################################################################################
+    # Function to validate command line options given by user                                #
+    ##########################################################################################
     def validate_and_process_command_options(self,opts):
-        def folder_option_handler(opt_val):
-            self.is_output_file = True
+        def folder_option_handler(opt_val): # Function to handle '--folder' option
+            self.is_file_output = True
             self.output_file = opt_val
-        def output_option_handler(opt_val):
+        def output_option_handler(opt_val): # Function to handle '-o' or '--output' option
             self.folder = opt_val
             if not os.path.exists(opt_val):
                 print(CONSTS.WRONG_FOLDER_MESSAGE)
@@ -80,8 +107,8 @@ class CSVCombiner():
                 else:
                     file_path = self.folder + "/" + csv_file
                 self.files.append(file_path)
-        def no_print_option_handler():
-            self.is_print = True
+        def no_print_option_handler(): # Function to handle '--no-print' option
+            self.is_print = False
         for opt in opts:
             opt_name = opt[0]
             opt_val = opt[1]
@@ -92,9 +119,15 @@ class CSVCombiner():
             elif opt_name == "--no-print":
                 no_print_option_handler()
     
+    ##########################################################################################
+    # Function to combine csv files                                                          #
+    ##########################################################################################
     def combine_csv(self):
         self.combined_csv = pd.concat(self.file_chunks)
 
+    ##########################################################################################
+    # Function to create output csv file                                                     #
+    ##########################################################################################
     def create_csv_file(self):
         self.combined_csv.to_csv(self.output_file+".csv",index=False,header=True)
 
