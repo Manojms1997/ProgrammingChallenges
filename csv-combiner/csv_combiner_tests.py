14591250e487497a17e3ed93241f408a40361d97
ManojMS <manojsrinath97@gmail.com>
2022-11-01 04:09:44 -0700

    MINOR CHANGES

--- csv-combiner/csv_combiner_tests.py
+++ csv-combiner/csv_combiner_tests.py
@@ -9,11 +9,10 @@ import generatefixtures
 class CSVTest(unittest.TestCase):
 
     csv_combiner = CSVCombiner()
-    accessories = "./fixtures/accessories.csv"
-    clothing = "./fixtures/clothing.csv"
-    household = "./fixtures/household_cleaners.csv"
-    op = "./op.csv"
-    op1 = './op1.csv'
+    ACCESSORIES = "./fixtures/accessories.csv"
+    CLOTHING = "./fixtures/clothing.csv"
+    HOUSEHOLD = "./fixtures/household_cleaners.csv"
+    OP = "./op.csv"
 
     @classmethod
     def setUpClass(cls):
@@ -21,15 +20,15 @@ class CSVTest(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        os.remove(cls.accessories)
-        os.remove(cls.clothing)
-        os.remove(cls.household)
+        os.remove(cls.ACCESSORIES)
+        os.remove(cls.CLOTHING)
+        os.remove(cls.HOUSEHOLD)
         # os.remove(cls.op)
         os.rmdir("./fixtures")
     
     def tearDown(self):
-        if os.path.exists(self.op):
-            os.remove(self.op)
+        if os.path.exists(self.OP):
+            os.remove(self.OP)
 
     def test_no_arg(self):
         print("test to check when no arguments are passed to the command line")
@@ -59,21 +58,21 @@ class CSVTest(unittest.TestCase):
     
     def test_combine_two_csv_length(self):
         print("test to check the length of combined csv for two input csv")
-        self.csv_combiner.main(['-o', 'op', '--no-print', self.accessories, self.clothing])
+        self.csv_combiner.main(['-o', 'op', '--no-print', self.ACCESSORIES, self.CLOTHING])
         op_df = pd.read_csv("./op.csv")
         op_rows = op_df.shape[0]
-        accessories_df = pd.read_csv(self.accessories)
+        accessories_df = pd.read_csv(self.ACCESSORIES)
         accessories_rows = accessories_df.shape[0]
-        clothing_df = pd.read_csv(self.clothing)
+        clothing_df = pd.read_csv(self.CLOTHING)
         clothing_rows = clothing_df.shape[0]
         self.assertEqual(op_rows,accessories_rows+clothing_rows,"output csv rows doesn't match")
     
     def test_new_col_added(self):
         print("test to check if a new column is added to the combined csv")
-        self.csv_combiner.main(['-o', 'op', '--no-print', self.accessories, self.clothing])
+        self.csv_combiner.main(['-o', 'op', '--no-print', self.ACCESSORIES, self.CLOTHING])
         op_df = pd.read_csv("./op.csv")
         op_cols = op_df.shape[1]
-        accessories_df = pd.read_csv(self.accessories)
+        accessories_df = pd.read_csv(self.ACCESSORIES)
         accessories_cols = accessories_df.shape[1]
         self.assertEqual(op_cols,accessories_cols+1,"output csv cols doesn't match")
 
@@ -83,36 +82,36 @@ class CSVTest(unittest.TestCase):
         csv_comb.main(['-o', 'op', '--no-print', '--folder', './fixtures'])
         op_df = pd.read_csv("./op.csv")
         op_rows = op_df.shape[0]
-        accessories_df = pd.read_csv(self.accessories)
+        accessories_df = pd.read_csv(self.ACCESSORIES)
         accessories_rows = accessories_df.shape[0]
-        clothing_df = pd.read_csv(self.clothing)
+        clothing_df = pd.read_csv(self.CLOTHING)
         clothing_rows = clothing_df.shape[0]
-        household_df = pd.read_csv(self.household)
+        household_df = pd.read_csv(self.HOUSEHOLD)
         household_rows = household_df.shape[0]
         self.assertEqual(op_rows,accessories_rows+clothing_rows+household_rows,"output csv cols doesn't match")
 
     def test_combine_three_csv_length(self):
         print("test to check if three csv files are combined")
         csv_comb = CSVCombiner()
-        csv_comb.main(['-o', 'op', '--no-print', self.household,self.accessories,self.clothing])
+        csv_comb.main(['-o', 'op', '--no-print', self.HOUSEHOLD,self.ACCESSORIES,self.CLOTHING])
         op_df = pd.read_csv("./op.csv")
         op_rows = op_df.shape[0]
-        accessories_df = pd.read_csv(self.accessories)
+        accessories_df = pd.read_csv(self.ACCESSORIES)
         accessories_rows = accessories_df.shape[0]
-        clothing_df = pd.read_csv(self.clothing)
+        clothing_df = pd.read_csv(self.CLOTHING)
         clothing_rows = clothing_df.shape[0]
-        household_df = pd.read_csv(self.household)
+        household_df = pd.read_csv(self.HOUSEHOLD)
         household_rows = household_df.shape[0]
         self.assertEqual(op_rows,accessories_rows+clothing_rows+household_rows,"output csv cols doesn't match")
 
     def test_csv_files_combine_value(self):
         print("test to check if combined csv has same contents as combining two input csvs and adding a column")
         csv_comb = CSVCombiner()
-        csv_comb.main(['-o', 'op', '--no-print', self.household,self.accessories])
+        csv_comb.main(['-o', 'op', '--no-print', self.HOUSEHOLD,self.ACCESSORIES])
         op_df = pd.read_csv("./op.csv")
-        household_df = pd.read_csv(self.household)
+        household_df = pd.read_csv(self.HOUSEHOLD)
         household_df['filename'] = 'household_cleaners.csv'
-        accessories_df = pd.read_csv(self.accessories)
+        accessories_df = pd.read_csv(self.ACCESSORIES)
         accessories_df['filename'] = 'accessories.csv'
         combined_df = pd.concat([household_df,accessories_df])
         self.assertEqual(op_df.values.tolist(),combined_df.values.tolist())
